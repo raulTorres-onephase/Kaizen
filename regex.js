80899d90549c7972a86b37e3505f055c6547568a
raul.torres <raul.torres@onephase.com>
2020-11-09 00:25:07 -0700

    created regex.js file

---
+++ regex.js
@@ -0,0 +1,3 @@
+// 16. You should be able to detect a number in a string 
+
+// 17. You should be able to determine whether a string matches a pattern => the pattern is XXX-XXX-XXXX where all X's are digits  
