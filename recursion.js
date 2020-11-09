bf8e8c7e1b29b8c10298d53030faf218e46a87ed
raul.torres <raul.torres@onephase.com>
2020-11-08 22:25:54 -0700

    continuing with exercise 12

--- recursion.js
+++ recursion.js
@@ -1,36 +1,37 @@
-var fileData = {	
-    dir: 'app',	
-    files: [	
-      'index.html',	
-      {	
-        dir: 'js',	
-        files: [	
-          'main.js',	
-          'app.js',	
-          'misc.js',	
-          {	
-            dir: 'vendor',	
-            files: [	
-              'jquery.js',	
-              'underscore.js'	
-            ]	
-          }	
-        ]	
-      },	
-      {	
-        dir: 'css',	
-        files: [	
-          'reset.css',	
-          'main.css'	
-        ]	
-      }	
-    ]
-}
-// 11. You should be able to return a list of files from the fileData, which can be found at tab Recursion Json
+var fileData = {
+  dir: 'app',
+  files: [
+    'index.html',
+    {
+      dir: 'js',
+      files: [
+        'main.js',
+        'app.js',
+        'misc.js',
+        {
+          dir: 'vendor',
+          files: ['jquery.js', 'underscore.js'],
+        },
+      ],
+    },
+    {
+      dir: 'css',
+      files: ['reset.css', 'main.css'],
+    },
+  ],
+};
+//11. You should be able to return a list of files from the fileData, which can be found at tab Recursion Json
 
-function getFiles(obj){
-  let files = []
-  
-}
+const getFiles = ({ files }) => {
+  let allFiles = [];
+  files.forEach((file) => {
+    if (typeof file === 'string') {
+        allFiles.push(file);
+    } else {
+      getFiles(file);
+    }
+  });
+  return allFiles;
+};
 
-console.log(getFiles(fileData))
+console.log(getFiles(fileData, 'app'))
