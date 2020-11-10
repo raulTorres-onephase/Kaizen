1a12c9eaff53c572c68d8920e3e6a53c5492b164
raul.torres <raul.torres@onephase.com>
2020-11-09 23:23:18 -0700

    Done with exercises 16 & 17

--- regex.js
+++ regex.js
@@ -1,3 +1,24 @@
 // 16. You should be able to detect a number in a string 
+// const x = 'we are number one'
+// const detectANumber = (str) => {
+//     let strToBeChecked = str
+//     let checkPoint = /\d/g;
+//     let result = checkPoint.test(strToBeChecked)
+//     return result
+// }
+//console.log(detectANumber(x))
 
 // 17. You should be able to determine whether a string matches a pattern => the pattern is XXX-XXX-XXXX where all X's are digits  
+const number = '634-504-2139' 
+const detectPattern = (str) =>{
+    let numToBeChecked = str
+    let checkPoint = /^\d{3}\-\d{3}\-\d{4}$/;
+    let result = checkPoint.test(numToBeChecked)
+    if(!result){
+        return 'The strig does not match the pattern or has letters in it'
+    } else {
+        return 'The pattern is correct'
+    }
+}
+
+console.log(detectPattern(number))
