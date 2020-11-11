b7563a94d31d7a2964293161a9bba617d700dc2e
raul.torres <raul.torres@onephase.com>
2020-11-10 23:47:10 -0700

    added exercise #9 with minimun validations

--- numbers.js
+++ numbers.js
@@ -1,7 +1,9 @@
 // 9. Given a base10 number you should be able to find the bit value at a given position  
-let num = 233
-const getValueAndPosition = (number, result = []) => {
+const data = 233;
+const pos = 8
+const getValueAndPosition = (number, result = [], position) => {
     let base = 2
+    let findIndex = position; 
     let calculateRemainder = (number % base)
     result.push(calculateRemainder)
     let divisionNumber = Math.floor(number / base)
@@ -10,11 +12,15 @@ const getValueAndPosition = (number, result = []) => {
     } else {
         getValueAndPosition(divisionNumber, result)
     }
-    let resultBeautify = result.toString().replace(/,/, '')
-    return `The binary number is (${resultBeautify})`
+    let pos = result.filter((value, i) => (i + 1 === findIndex))
+    if(findIndex > result.length){
+        return `${findIndex} is too high, try a lower number!`
+    }
+    let resultBeautify = result.toString().replace(/,/g, '')
+    return `The binary number is ${resultBeautify} and the position at #${findIndex} is ${pos}`
 }
 
-console.log(getValueAndPosition(num))
+console.log(getValueAndPosition(data, [] ,pos))
 
 // 10. Return the base10 representation of a binary string
 
