84c062d1c9cf3ad386016ad6ce53d8ef44c3e6a8
raul.torres <raul.torres@onephase.com>
2020-11-11 21:56:34 -0700

    fixed issues on numbers.js

--- numbers.js
+++ numbers.js
@@ -1,19 +1,19 @@
 // Creating personal reverse function
 const ownReverse = (arr) => {
-    let newArr = []  
-    let long = arr.length;
-    for (let i = 0; i < arr.length; i++) {
-      let rev = (long - i) - 1;
-      newArr.push(arr[rev]);
-    }
-    return newArr
-  };
-  
-  //console.log(ownReverse(test));
+  let newArr = [];
+  let long = arr.length;
+  for (let i = 0; i < arr.length; i++) {
+    let rev = long - i - 1;
+    newArr.push(arr[rev]);
+  }
+  return newArr;
+};
+
+//console.log(ownReverse(test));
 
 // 9. Given a base10 number you should be able to find the bit value at a given position
-const data = 233;
-const pos = 8;
+const data = 500;
+const pos = 3;
 const getValueAndPosition = (number, result = [], position) => {
   let base = 2;
   let findIndex = position;
@@ -25,22 +25,23 @@ const getValueAndPosition = (number, result = [], position) => {
   } else {
     getValueAndPosition(divisionNumber, result);
   }
-  let pos = result.filter((value, i) => i + 1 === findIndex);
+  let pos = ownReverse(result).filter((value, i) => {
+    return findIndex === i + 1;
+  });
   if (findIndex > result.length) {
     return `${findIndex} is too high, try a lower number!`;
   }
-  let resultBeautify = result.toString().replace(/,/g, '');
+  let resultBeautify = ownReverse(result).toString().replace(/,/g, '');
   return `The binary number is ${resultBeautify} and the position at #${findIndex} is ${pos}`;
 };
 
-//console.log(getValueAndPosition(data, [] ,pos))
+console.log(getValueAndPosition(data, [], pos));
 
 // 10. Return the base10 representation of a binary string
 
 const convertTo10 = (binary) => {
   let poweredResult = 0;
-  const arr = Array.from(String(binary), Number);
-  ownReverse(arr);
+  const arr = ownReverse(Array.from(String(binary), Number));
   arr.forEach((digit, i) => {
     let powered = Math.pow(2, i) * digit;
     poweredResult += powered;
@@ -48,6 +49,4 @@ const convertTo10 = (binary) => {
   return poweredResult;
 };
 
-console.log(convertTo10(10010111))
-
-
+//console.log(convertTo10(1100001001))
