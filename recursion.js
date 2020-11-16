742e9c590f08c863c0554a244951b3c458708747
raul.torres <raul.torres@onephase.com>
2020-11-16 01:12:53 -0700

    finishing with first exercises, moving on to last 4

--- recursion.js
+++ recursion.js
@@ -22,15 +22,23 @@ var fileData = {
 };
 //11. You should be able to return a list of files from the fileData, which can be found at tab Recursion Json
 
-const getFiles = ({ files }, allFiles = []) => {
+const getFiles = (
+  { dir, files },
+  dirToSearch,
+  allFiles = [],
+  firstIter = false
+) => {
+  let appendDir = firstIter || dir === dirToSearch;
   files.forEach((file) => {
     if (typeof file === 'string') {
+      if (appendDir) {
         allFiles.push(file);
+      }
     } else {
-      getFiles(file, allFiles);
+      getFiles(file, dirToSearch, allFiles, appendDir);
     }
   });
   return allFiles;
 };
 
-console.log(getFiles(fileData))
+console.log(getFiles(fileData));
